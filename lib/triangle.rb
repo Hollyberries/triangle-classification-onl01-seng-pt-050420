96424f62aa59b02c83ccd09bcce2550ebbc35722
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-10 22:22:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,24 +18,17 @@ class Triangle
       :scalene
   end
 end
-  
-  def validate_triangle
-    
+
+def valid_triangle
+  triangle = [(a + b > c), (a + c > b), (b + c > a)]
+  [a, b, c].each do |side|
+  triangle << false if side <= 0 
+  raise TriangleError if triangle.include?(false)
   end
-  
-  
 end
 
+class TriangleError < StandardError
+end
 
-#  def validate_triangle
-#    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-#   [a, b, c].each do |side|
-#      real_triangle << false if side <= 0 
-#    raise TriangleError if real_triangle.include?(false)
-#    end
-#  end
-
-#  class TriangleError < StandardError
-#  end
-
-#end
+  
+end
