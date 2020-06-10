82426beed77caec384617279377751acfb0150c2
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-10 22:18:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,23 +20,12 @@ class Triangle
 end
   
   def validate_triangle
-  class TriangleError < StandardError
+    
   end
   
   
 end
 
-
-#  def kind
-#   validate_triangle
-#    if a == b && b == c
-#      :equilateral
-#    elsif a == b || b == c || a == c
-#      :isosceles
-#    else
-#      :scalene
-#    end
-#  end
 
 #  def validate_triangle
 #    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
